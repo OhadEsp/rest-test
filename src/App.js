92b26136971b5990724e5c9617a12eec401a7d8c
Ohad <ohad999@gmail.com>
2019-12-07 11:42:00 +0200

    commiting old rest-test

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import './App.css';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  const [url, setUrl] = useState('');
+  const [data, setData] = useState('');
+
+  return (
+    <div className="App">
+    {/* //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    //  */}
+      <TextField fullWidth onChange={e => setUrl(e.target.value)}></TextField>
+      <Button variant="contained" color="primary" onClick={async () => {
+        const data = await getData(url);
+        setData(data);
+        // console.log(JSON.stringify(data));
+      }}>Hello World</Button>
+      <div>{JSON.stringify(data)}</div>
+    </div>
+  );
+}
+
+async function getData(url) {
+  const response = await fetch(url);
+  return response.json();
+}
+
+export default App;
