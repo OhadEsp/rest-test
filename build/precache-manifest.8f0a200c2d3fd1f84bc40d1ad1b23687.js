1d621c8990dfc62c244a370ccace5759a579648d
Ohad <ohad999@gmail.com>
2019-12-13 01:11:47 +0200

    deployed

---
+++ build/precache-manifest.8f0a200c2d3fd1f84bc40d1ad1b23687.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3e95545c4deee6765c5a58a754dbd24",
+    "url": "/rest-test/index.html"
+  },
+  {
+    "revision": "99dc5f46614426e3c2c4",
+    "url": "/rest-test/static/css/main.7d416a98.chunk.css"
+  },
+  {
+    "revision": "fe925c272edca2eba3d4",
+    "url": "/rest-test/static/js/2.2dafc377.chunk.js"
+  },
+  {
+    "revision": "99dc5f46614426e3c2c4",
+    "url": "/rest-test/static/js/main.593bde15.chunk.js"
+  },
+  {
+    "revision": "2e31da4e05a1cdd97cb0",
+    "url": "/rest-test/static/js/runtime-main.d686c60b.js"
+  }
+]);
