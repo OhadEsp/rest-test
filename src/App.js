d2f9e6039e28e5d9563094e3c796fe4faf5788e3
Ohad <ohad999@gmail.com>
2019-12-14 15:17:26 +0200

    Added heading and little styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Table from './Table.jsx';
+import Box from '@material-ui/core/Box';
+import { Typography } from '@material-ui/core';
 
 function App() {
   const [url, setUrl] = useState('');
@@ -10,15 +12,29 @@ function App() {
   let displayed = data !== undefined ? <Table data={data} /> : <div></div>;
 
   return (
-    <div className="App">
-      <TextField fullWidth onChange={e => setUrl(e.target.value)}></TextField>
-      <Button variant="contained" color="primary" onClick={async () => {
-        const data = await getData(url);
-        setData(data);
-        console.log(JSON.stringify(data));
-      }}>Get Data</Button>
-      <div>{displayed}</div>
-    </div>
+    <Box>
+      <Box display="flex" alignItems="center" justifyContent="center" p={1}>
+        <Typography variant="h4">
+          Welcome to my Rest-API-Test App
+        </Typography>
+      </Box>
+      
+      <Box display="flex" alignItems="center" justifyContent="center" p={1}>
+        <TextField id="filled-basic" variant="filled" onChange={e => setUrl(e.target.value)} fullWidth></TextField>
+      </Box>
+
+      <Box display="flex" alignItems="center" justifyContent="center" p={1}>
+        <Button variant="contained" color="primary" onClick={async () => {
+          const data = await getData(url);
+          setData(data);
+          console.log(JSON.stringify(data));
+        }}>Get Data</Button>
+      </Box>
+      
+      <Box display="flex" alignItems="center" justifyContent="center" p={1}>
+        {displayed}
+      </Box>
+    </Box>
   );
 }
 
